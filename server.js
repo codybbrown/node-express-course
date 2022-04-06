54078c9ebc4e521f37103a3471b7555421bc7166
Cody Brown <cody@brightinfinite.com>
2022-04-05 23:31:40 -0500

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
+const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }]
+
+app.get('/users', function (req, res) {
+	res.json({
+		success: true,
+		message: 'successfully got users. Buenus.',
+		users: mockUserData,
+	})
+})
+
 app.listen(port, function () {
 	console.log(`Server is running on ${port}`)
 })
