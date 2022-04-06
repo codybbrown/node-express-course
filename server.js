96f6ee262c60b2b85ba6633787f0892a41176728
Cody Brown <cody@brightinfinite.com>
2022-04-06 01:16:40 -0500

    add dynamic GET route

--- server.js
+++ server.js
@@ -5,11 +5,21 @@ const port = process.env.PORT || 8000
 
 const mockUserData = [{ name: 'Mark' }, { name: 'Jill' }]
 
+// API Endpoint: /users
 app.get('/users', function (req, res) {
 	res.json({
 		success: true,
 		message: 'successfully got users. Buenus.',
 		users: mockUserData,
+	})
+})
+
+app.get('/users/:id', function (req, res) {
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: `got 1 user for ya here bubs`,
+		user: req.params.id,
 	})
 })
 
