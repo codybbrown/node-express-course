e88fc83e2296e50514fdd253d89c8184fed90e4e
Cody Brown <cody@brightinfinite.com>
2022-04-06 01:34:22 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 8000
 
@@ -14,6 +17,7 @@ app.get('/users', function (req, res) {
 	})
 })
 
+// words with colon in front in url are used to indicate a variable and can be accessed with req.params.variable
 app.get('/users/:id', function (req, res) {
 	console.log(req.params.id)
 	res.json({
@@ -21,6 +25,30 @@ app.get('/users/:id', function (req, res) {
 		message: `got 1 user for ya here bubs`,
 		user: req.params.id,
 	})
+})
+
+app.post('/login', function (req, res) {
+	// Passwords are encrypted using something like bcrypt before being sent to the server
+	const username = req.body.username
+	const password = req.body.password
+
+	// Will come from database
+	const mockUserName = 'Boffin'
+	const mockPassword = 'rim-explorer'
+
+	if (username === mockUserName && password === mockPassword) {
+		// In practice, use JSON web token sign method here to make an encrypted token
+		res.json({
+			success: true,
+			message: 'You are logged in',
+			token: 'encryptid token goes here',
+		})
+	} else {
+		res.json({
+			success: false,
+			message: 'Password an uname no not match',
+		})
+	}
 })
 
 app.listen(port, function () {
